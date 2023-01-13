31d00259596f5a35fe02e65eb32167a01d414487
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2023-01-13 16:00:35 -0500

    msg

--- Lab.asm
+++ Lab.asm
@@ -41,6 +41,12 @@ section .text                               ;Main code
         PUSH r8
         call pow
         POP r9
+        mov [temp_var], r9             ;Save remainder of division to variable, for printing
+        mov rax, 1                      ;System write 
+        mov rdi, 1                      ;Stdout 
+        mov rsi, temp_var               ;Message to be sent, in this case, rdx
+        mov rdx, temp_var_len           ;Message length
+        syscall                         ;Print message
         pnumwork:                           ;Looping function, to prevent POP rbx from looping
             POP rax                         ;Get integer from the stack
             mov rdx, 0                      ;Clear rdx
