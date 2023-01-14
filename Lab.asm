461a9a19ac9956c749f0871219439f7f2516733e
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2023-01-14 17:14:48 -0500

    msg

--- Lab.asm
+++ Lab.asm
@@ -83,8 +83,9 @@ section .text                               ;Main code
         POP r8                              ;Save return adress to r8
         POP r9                              ;Save exponent to r9
         dec r9                              ;Decrement exponent, for compatibility with recursive function
-        PUSH r9
-        call pnum
+        mov eax, 60
+        mov rdi, r9
+        syscall
         POP r10                             ;Save base to r10
         mov r11, r10                        ;Make r11 equal to r10
         powwork:                            ;Recursive function
