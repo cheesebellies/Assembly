26d7e9cc74161e97e6997b2b9c45004abaf4d3c4
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2023-01-14 14:25:39 -0500

    msg

--- Lab.asm
+++ Lab.asm
@@ -32,119 +32,74 @@ factor_start_length_2: equ $-factor_start_message_2
 section .text                               ;Main code
 
 
-    pnum2:
-        POP r15         ;ret
-        POP r14         ;num
-        PUSH r14
-        call cdigits    
-        POP rbx         ;len
-        dec rbx
-        mov r8, 10      ;pownum
-        PUSH r8         
-        PUSH rbx
-        call pow
-        POP r8          ;powresult
-        mov r9, 10
-        pnum2work:
-            mov rax, r14
-            xor rdx, rdx
-            div r8
-            mov r10, rax
-            imul rax, r8
-            sub r14, rax
-            mov rax, r10
-            add rax, 48
-            mov [temp_var], rax             ;Save remainder of division to variable, for printing
+    pnum:                                   ;Number printing function
+        POP r15                             ;Saving return adress in r15
+        POP r14                             ;Saving number to be printed in r14
+        PUSH r14                            ;Saving number to be printed on stack
+        call cdigits                        ;Counting digits of number
+        POP rbx                             ;Saving number of digits to rbx
+        dec rbx                             ;Decrementing it, so it functions well with power function
+        mov r8, 10                          ;Saving 10 to r8
+        PUSH r8                             ;Saving 10 to stack
+        PUSH rbx                            ;Saving number of digits to stack
+        call pow                            ;Getting 10^number of digits, for use in printing function
+        POP r8                              ;Saving that number to r8
+        mov r9, 10                          ;Saving 10 in r9
+        pnumwork:                           ;Recursive function for actually printing number
+            mov rax, r14                    ;Saving number to print (r14) to rax
+            xor rdx, rdx                    ;Making rdx 0, so division doesnt mess up
+            div r8                          ;Dividing rax by r8
+            mov r10, rax                    ;Saving result to r10
+            imul rax, r8                    ;Multiplying result with r8, so we can subtract it from the number we're printing. EG: num is 123, r8 is 100, rax is 1, then subtracting 100 from 123, so we can recurse with 23 as num.
+            sub r14, rax                    ;Subtracting multiplication result from num
+            mov rax, r10                    ;Moving result back to rax
+            add rax, 48                     ;Adding 48 to rax for unicode, otherwise, printing, for example, 1, wouldent print one but unicode char with code 1
+            mov [temp_var], rax             ;Save result of division to variable, for printing
             mov rax, 1                      ;System write 
             mov rdi, 1                      ;Stdout 
-            mov rsi, temp_var               ;Message to be sent, in this case, rdx
+            mov rsi, temp_var               ;Message to be sent
             mov rdx, temp_var_len           ;Message length
-            syscall
-            xor rdx, rdx
-            mov rax, r8
-            div r9
-            mov r8, rax
-            mov rax, r14
-            cmp rax, 0
-            jnz pnum2work
-        PUSH r15
-        ret
+            syscall                         ;Printing message
+            xor rdx, rdx                    ;Making rdx 0
+            mov rax, r8                     ;Making rax r8
+            div r9                          ;Dividing r8 by 10
+            mov r8, rax                     ;Making r8 the division result, so when recursing we keep up with each digit in the number to print
+            mov rax, r14                    ;Moving number to print to rax
+            cmp rax, 0                      ;Comparing it with zero
+            jnz pnumwork                    ;If it is zero, stop recursing, else, recurse
+        PUSH r15                            ;Pushing return address to top of stack
+        ret                                 ;Return to location the function was called from
 
+    cdigits:                                ;Digit counting function
+        POP rbx                             ;Saving return adress to rbx
+        POP rax                             ;Getting number to count digits of, saving to rax
+        mov r8, 10                          ;Making r8 10
+        mov r9, 0                           ;Making r9 0
+        cdigitswork:                        ;Recursing function
+            xor rdx, rdx                    ;Making rdx 0
+            div r8                          ;Dividing number to count digits of by 10
+            inc r9                          ;Incrementing r9 (number of digits counter variable)
+            cmp rax, 0                      ;Comparing division result with 0
+            jnz cdigitswork                 ;If rax is 0, exit recursive function, else, recurse.
+        PUSH r9                             ;Push result number to stack
+        PUSH rbx                            ;Push return adress to stack
+        ret                                 ;Return to location the function was called from
 
-
-
-    ; pnum:                                   ;Integer printing function. POP rdx as number to print
-    ;     POP rbx                             ;Save return adress in rbx
-    ;     call cdigits
-    ;     POP rax
-    ;     mov rbx, 10
-    ;     PUSH rax
-    ;     PUSH rbx
-    ;     call pow
-    ;     POP r9
-    ;     mov eax, 60
-    ;     POP r8
-    ;     mov rdi, r8
-    ;     syscall
-    ;     pnumwork:                           ;Looping function, to prevent POP rbx from looping
-    ;         POP rax                         ;Get integer from the stack
-    ;         mov rdx, 0                      ;Clear rdx
-    ;         mov r10, 10
-    ;         div r9                          ;Divide rax by r8, remainder goes into rdx, quotient into rax
-    ;         add rdx, 48                     ;Add 48 to remainder so the number will function with 
-    ;         PUSH rax                        ;Save rax to the stack so it isn't overwritten by printing function
-    ;         mov [temp_var], rdx             ;Save remainder of division to variable, for printing
-    ;         mov rax, 1                      ;System write 
-    ;         mov rdi, 1                      ;Stdout 
-    ;         mov rsi, temp_var               ;Message to be sent, in this case, rdx
-    ;         mov rdx, temp_var_len           ;Message length
-    ;         syscall                         ;Print message
-    ;         POP rax                         ;Retrieve saved quotient
-    ;         mov r8, rax                     ;Save rax to r8
-    ;         PUSH r8                         ;Save r8 on stack
-    ;         mov rdx, 0
-    ;         mov rax, r9
-    ;         div r10
-    ;         mov r9, rax
-    ;         POP rax
-    ;         mov r8, rax
-    ;         PUSH r8
-    ;         cmp rax, 0                      ;Compare rax with zero
-    ;         jnz pnumwork                    ;Repeat at pnumwork if rax isn't zero
-    ;     PUSH rbx                            ;Save return adress to stack
-    ;     ret                                 ;Return to call location
-
-    cdigits:
-        POP rbx
-        POP rax
-        mov r8, 10
-        mov r9, 0
-        mov r10, 0
-        cdigitswork:
-            mov rdx, 0
-            div r8
-            inc r9
-            cmp rax, r10
-            jnz cdigitswork
-        PUSH r9
-        PUSH rbx
-        ret
-
-    pow:
-        POP r8  ;ret
-        POP r9  ;pow
-        dec r9
-        POP r10 ;num
-        mov r11, r10
-        mov rcx, r9
-        powwork:
-            imul r10, r11
-            dec r9
-            cmp r9, 0
-            jne powwork
-        PUSH r10
-        PUSH r8
-        ret
+    pow:                                    ;Function to get a number to the power of another
+        POP r8                              ;Save return adress to r8
+        POP r9                              ;Save exponent to r9
+        dec r9                              ;Decrement exponent, for compatibility with recursive function
+        POP r10                             ;Save base to r10
+        mov r11, r10                        ;Make r11 equal to r10
+        mov rcx, r9                         ;Move rcx to r9 (most likely this is vestigial, im not messing with it just in case.)
+        powwork:                            ;Recursive function
+            imul r10, r11                   ;Multiply base by copy of itself
+            dec r9                          ;Decrement loop counter
+            cmp r9, 0                       ;Compare loop counter with zero
+            jne powwork                     ;If so, exit recursive function, else, recurse
+        PUSH r10                            ;Push result to stack
+        PUSH r8                             ;Push return adress to stack
+        ret                                 ;Return to location the function was called from
             
 
 
@@ -157,7 +112,7 @@ section .text                               ;Main code
         mov rsi, factor_start_message_1     ;Message to be sent
         mov edx, factor_start_length_1      ;Message length
         syscall                             ;Print message
-        mov r8, 234
+        mov r8, 50
         PUSH r8
         call pnum2                          ;Print number in r8
         mov    eax, 1                       
@@ -171,10 +126,7 @@ section .text                               ;Main code
 
 
     _start:                                 ;Linker instruction, code starts execution here
-        mov rax, 123
-        PUSH rax
-        call pnum2
-        ; call factors
+        call factors
 
 
 ;********************************************************
